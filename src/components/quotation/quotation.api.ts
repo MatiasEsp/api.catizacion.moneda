44ffdb1c3bc4157c0c3fdf379926c4f0e81bcac8
Matiase <matias.f.espinoza@gmail.com>
2020-11-30 12:11:00 -0300

    create QuotationApi, QuotationController

---
+++ src/components/quotation/quotation.api.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { asyncMdl } from '../../common/asyncMdl';
+import { QuotationController } from './quotation.controller';
+
+const router = Router();
+
+const api = '/cotizacion';
+
+router.get(`${api}/dolar`, asyncMdl(QuotationController.getQuotationDolar));
+router.get(`${api}/euro`, asyncMdl(QuotationController.getQuotationEuro));
+router.get(`${api}/real`, asyncMdl(QuotationController.getQuotationReal));
---
+++ src/components/quotation/quotation.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import { QuotationDTO } from './dtos/quetation.dto';
+import { QuatationService } from './quotation.service';
+
+const getQuotationDolar = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quotationDto: QuotationDTO = await QuatationService.serviceExtractValueDolar();
+    return res.json(quotationDto);
+  } catch (error) {
+    return res.status(400).json({ status: 'error', msjError: '' });
+  }
+};
+const getQuotationEuro = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quotationDto: QuotationDTO = await QuatationService.serviceExtractValueEuro();
+    return res.json(quotationDto);
+  } catch (error) {
+    return res.status(400).json({ status: 'error', msjError: '' });
+  }
+};
+const getQuotationReal = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const quotationDto: QuotationDTO = await QuatationService.serviceExtractValueReal();
+    return res.json(quotationDto);
+  } catch (error) {
+    return res.status(400).json({ status: 'error', msjError: '' });
+  }
+};
+
+export const QuotationController = {
+  getQuotationEuro,
+  getQuotationDolar,
+  getQuotationReal,
+}
